5a8c1de379ca11b40e8378c1040ca2fc00d91e1f
viradm334 <viradamayanti@student.telkomuniversity.ac.id>
2023-12-11 20:53:14 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Http\Controllers\CategoryController;
+use App\Models\Product;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home', [
+        "title" => "Home",
+        "active" => "home"
+    ]);
+});
+
+//Route::get('/products', [ProductController::class, 'index']);
+
+//Route::get('/products/{product:slug}', [ProductController::class, 'show']);
+
+Route::get('/login', [LoginController::class, 'index']);
+
+Route::get('/register', [RegisterController::class, 'index']);
+
+//Route::post('/register', [RegisterController::class, 'store']);
+
+/* Route::get('/dashboard', function(){
+    return view('dashboard.index');
+});
+
+Route::resource('/dashboard/categories', [CategoryController::class, 'index']);
+
+Route::resource('/dashboard/users', [UserController::class, 'index']);
+
+Route::resource('/dashboard/products', [ProductController::class, 'index']); */
+
